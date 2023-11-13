e94b6f9a8f99207551a7dc68c51a01299b421bfb
John <13438710+ekalnhoj@users.noreply.github.com>
2023-11-13 17:54:35 -0600

    Create shapes.py
    Some simple shapes with some inheritance happening. Easy to parse, hopefully that's the case for LLMs too.

---
+++ shapes.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+class Shape:
+    name = "Generic"
+    def __init__(self):
+        pass
+
+    def get_area(self):
+        # raise NotImplementedError
+        return float(-1)
+
+    def get_perimeter(self):
+        # raise NotImplementedError
+        return float(-1)
+
+class Circle(Shape):
+    name = "Circle"
+    def __init__(self,r):
+        self.r = float(r)
+
+    def get_area(self):
+        return np.pi * self.r**2
+
+    def get_perimeter(self):
+        return 2 * np.pi * self.r
+
+class Rectangle(Shape):
+    name = "Rectangle"
+    def __init__(self,l,w):
+        self.l = float(l)
+        self.w = float(w)
+
+    def get_area(self):
+        return self.l * self.w
+
+    def get_perimeter(self):
+        return 2*self.l + 2*self.w
+
+class Square(Rectangle):
+    name = "Square"
+    def __init__(self,l):
+        super().__init__(l=l,w=l)
+        
+class UnitSquare(Square):
+    name = "1-Square"
+    def __init__(self):
+        super().__init__(l=1)
+
+
+if __name__ == "__main__":
+    sha = Shape()
+    
+    circ = Circle(1)
+
+    rect = Rectangle(2,3)
+
+    rect_s = Rectangle(5,5)
+
+    sq = Square(5)
+
+    shapes = [sha,circ,rect,rect_s,sq]
+
+    for sh in shapes:
+        print(f"Shape: {sh.name : >10}\tArea: {sh.get_area():>02.2f}\tPeri: {sh.get_perimeter():>02.2f}")
